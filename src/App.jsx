393e4dd33e3fdf0757a846a69c8b92832076cd1c
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-08-08 18:28:35 +1000

    Drag and Drop: the beginning

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,27 @@ import SolitaireContext from "./lib/context";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, null, init);
+
+  const onClickYellow = () => {
+    console.log("clicked yellow");
+  };
+
+  const onClickRed = () => {
+    console.log("clicked red");
+  };
+
   return (
-    <SolitaireContext.Provider value={{ state, dispatch }}>
-      <div className="flex gap-6 flex-wrap">
-        <Field />
-      </div>
-    </SolitaireContext.Provider>
+    // <SolitaireContext.Provider value={{ state, dispatch }}>
+    //   <div className="flex gap-6 flex-wrap">
+    //     <Field />
+    //   </div>
+    // </SolitaireContext.Provider>
+    <div
+      className="w-64 h-64 bg-yellow-400 absolute left-12 top-12"
+      onClick={onClickYellow}
+    >
+      <div className="w-32 h-32 m-12 bg-red-500" onClick={onClickRed}></div>
+    </div>
   );
 }
 
