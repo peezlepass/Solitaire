37c013aa86b49977660474bd6beaed76286a4f03
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-08-08 19:04:21 +1000

    D&D: Yellow + onCLick + onMouseDown + onMouseUp

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,16 @@ import SolitaireContext from "./lib/context";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, null, init);
-
-  const onClickYellow = (event) => {
-    event.stopProoagation();
-    console.log("clicked yellow");
+  const onClick = () => {
+    console.log("clicked yellow!");
   };
 
-  const onClickRed = () => {
-    console.log("clicked red");
+  const onMouseDown = () => {
+    console.log("mouse down");
   };
 
-  const onClickWrapper = () => {
-    console.log("clicked wrapper");
+  const onMouseUp = () => {
+    console.log("mouse up");
   };
 
   return (
@@ -25,14 +23,12 @@ function App() {
     //     <Field />
     //   </div>
     // </SolitaireContext.Provider>
-    <div onClick={onClickWrapper}>
-      <div
-        className="w-64 h-64 bg-yellow-400 absolute left-12 top-12"
-        onClick={onClickYellow}
-      >
-        <div className="w-32 h-32 m-12 bg-red-500" onClick={onClickRed}></div>
-      </div>
-    </div>
+    <div
+      className="w-64 h-64 bg-yellow-400 absolute left-12 top-12"
+      onClick={onClick}
+      onMouseDown={onMouseDown}
+      onMouseUp={onMouseUp}
+    ></div>
   );
 }
 
