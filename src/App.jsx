ce42b75e83f65811721a66806cf236c821c1abe4
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-08-09 20:22:54 +1000

    D&D: pointer events

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import SolitaireContext from "./lib/context";
 function App() {
   // const [state, dispatch] = useReducer(reducer, null, init);
 
-  const [{ x, y }, changePosition] = useState({ x: 150, y: 150 });
+  const [{ x, y }, changePosition] = useState({ x: 200, y: 200 });
   const [{ xOffset, yOffset }, changeOffset] = useState({
     xOffset: 0,
     yOffset: 0,
   });
+  const [isDragging, setDragging] = useState(false);
 
   const onMouseDown = (downEvent) => {
+    setDragging(true);
     const rect = downEvent.target.getBoundingClientRect();
     changePosition({ x: downEvent.pageX, y: downEvent.pageY });
     changeOffset({
@@ -25,6 +27,7 @@ function App() {
     };
 
     const onMouseUp = (upEvent) => {
+      setDragging(false);
       window.removeEventListener("mousemove", onMouseMove);
       window.removeEventListener("mouseup", onMouseUp);
     };
@@ -33,17 +36,31 @@ function App() {
     window.addEventListener("mouseup", onMouseUp);
   };
 
+  const onDrop = () => {
+    if (isDragging) {
+      console.log("dropped me");
+    }
+  };
+
   return (
     // <SolitaireContext.Provider value={{ state, dispatch }}>
     //   <div className="flex gap-6 flex-wrap">
     //     <Field />
     //   </div>
     // </SolitaireContext.Provider>
-    <div
-      className="w-64 h-64 bg-yellow-400 absolute left-12 top-12"
-      style={{ left: x - xOffset, top: y - yOffset }}
-      onMouseDown={onMouseDown}
-    ></div>
+    <>
+      <div
+        className="w-96 h-96 bg-blue-300 absolute right-12 bottom-12"
+        onMouseUp={onDrop}
+      ></div>
+      <div
+        className={`w-64 h-64 bg-yellow-400 absolute ${
+          isDragging ? "pointer-events-none" : ""
+        }`}
+        style={{ left: x - xOffset, top: y - yOffset }}
+        onMouseDown={onMouseDown}
+      ></div>
+    </>
   );
 }
 
