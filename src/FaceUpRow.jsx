680de61be6cfbb5c4a2e03f64330724203a8d80a
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-08-22 13:42:15 +1000

    Fix not being able to drag the last card

--- src/FaceUpRow.jsx
+++ src/FaceUpRow.jsx
@@ -4,43 +4,42 @@ import Card from "./Card";
 
 export default function FaceUpRow({ cards, numberVisible }) {
   const { dispatch } = useContext(SolitaireContext);
+  const faceUpCards = cards.slice(cards.length - numberVisible, cards.length);
   return (
     <div className="flex -space-x-44 col-span-2">
-      {cards
-        .slice(cards.length - numberVisible, cards.length)
-        .map((cardDefinition, index) => {
-          let onMouseDown;
-          if (index === numberVisible - 1) {
-            onMouseDown = (event) => {
-              const { pageX, pageY, currentTarget } = event;
-              const { left, top } = currentTarget.getBoundingClientRect();
-              const action = {
-                type: "SELECT_CARD_FROM_FACE_UP_ROW",
-                payload: {
-                  selected: [cardDefinition],
-                  mouse: {
-                    x: pageX,
-                    y: pageY,
-                  },
-                  selectionOffset: {
-                    x: pageX - left,
-                    y: pageY - top,
-                  },
+      {faceUpCards.map((cardDefinition, index) => {
+        let onMouseDown;
+        if (index === faceUpCards.length - 1) {
+          onMouseDown = (event) => {
+            const { pageX, pageY, currentTarget } = event;
+            const { left, top } = currentTarget.getBoundingClientRect();
+            const action = {
+              type: "SELECT_CARD_FROM_FACE_UP_ROW",
+              payload: {
+                selected: [cardDefinition],
+                mouse: {
+                  x: pageX,
+                  y: pageY,
                 },
-              };
-              dispatch(action);
+                selectionOffset: {
+                  x: pageX - left,
+                  y: pageY - top,
+                },
+              },
             };
-          }
-          return (
-            <Card
-              key={`${cardDefinition.suit}-${cardDefinition.value}`}
-              value={cardDefinition.value}
-              suit={cardDefinition.suit}
-              faceUp={cardDefinition.faceUp}
-              onMouseDown={onMouseDown}
-            />
-          );
-        })}
+            dispatch(action);
+          };
+        }
+        return (
+          <Card
+            key={`${cardDefinition.suit}-${cardDefinition.value}`}
+            value={cardDefinition.value}
+            suit={cardDefinition.suit}
+            faceUp={cardDefinition.faceUp}
+            onMouseDown={onMouseDown}
+          />
+        );
+      })}
     </div>
   );
 }
